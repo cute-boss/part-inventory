a74c0696ecae1be715ffc3f4a8e78a9d223932d1
Azmir Sakri <33239728+cute-boss@users.noreply.github.com>
2024-07-09 15:58:08 +0800

    Add files via upload

---
+++ SPS/SPS/Scripts/custom.js
@@ -0,0 +1,25 @@
+﻿(function ($) {
+    "use strict"; // Start of use strict
+
+})(jQuery); // End of use strict
+
+
+// Event to remove violation when clicking image
+jQuery.event.special.touchstart = {
+    setup: function (_, ns, handle) {
+        if (ns.includes("noPreventDefault")) {
+            this.addEventListener("touchstart", handle, { passive: false });
+        } else {
+            this.addEventListener("touchstart", handle, { passive: true });
+        }
+    }
+};
+jQuery.event.special.touchmove = {
+    setup: function (_, ns, handle) {
+        if (ns.includes("noPreventDefault")) {
+            this.addEventListener("touchmove", handle, { passive: false });
+        } else {
+            this.addEventListener("touchmove", handle, { passive: true });
+        }
+    }
+};
